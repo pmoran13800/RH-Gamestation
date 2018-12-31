50a70b32cc478c2c2d36da348c3c7ca30ee5bcee
pmoran13800 <43425337+pmoran13800@users.noreply.github.com>
2018-12-31 18:54:40 -0500

    Changed theme names and relocated repo
    relocated to github.com/pmoran13800/rhgamestation-themes
    using RH_GAMESTATION and RH_CLASSIC themes in make script

--- package/batocera/emulationstation/recalbox-themes/recalbox-themes.mk
+++ package/batocera/emulationstation/recalbox-themes/recalbox-themes.mk
@@ -1,17 +1,17 @@
 ################################################################################
 #
-# Recalbox themes for EmulationStation : https://github.com/recalbox/recalbox-themes
+# RH Gamestation themes 
 #
 ################################################################################
 
 RECALBOX_THEMES_VERSION = master
-RECALBOX_THEMES_SITE = $(call github,batocera-linux,recalbox-themes,$(RECALBOX_THEMES_VERSION))
+RECALBOX_THEMES_SITE = $(call github,pmoran13800,rhgamestation-themes,$(RECALBOX_THEMES_VERSION))
 
 define RECALBOX_THEMES_INSTALL_TARGET_CMDS
         mkdir -p $(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
-	cp -r $(@D)/themes/recalbox \
+	cp -r $(@D)/themes/RH_GAMESTATION \
 		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
-	cp -r $(@D)/themes/batocera-light \
+	cp -r $(@D)/themes/RH_CLASSIC \
 		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 endef
 
